442df054ee3dfa9244e8c9f2f4faec9b3e5c6133
rama <ramastarzhu01@gmail.com>
2023-10-17 17:44:53 +0800

    配置tailwind for vue2不生效

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ module.exports = {
   // vue项目中关闭eslint
   lintOnSave: false,
   publicPath: './',
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [require('tailwindcss'), require('autoprefixer')]
+      }
+    }
+  }
 }
