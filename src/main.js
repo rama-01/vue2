32742fd1b8fd79df7357f7984e1e5c9af8946718
rama <ramastarzhu01@gmail.com>
2023-10-18 14:13:11 +0800

    引入axios拦截器

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import '@/plugins/element-ui'
-
 import router from '@/router'
+import request from '@/utils/request'
+import '@/plugins/element-ui'
+import '@/assets/styles/tailwind/index.css'
 
 // Vue.config.productionTip = false
-
-// 全部引入
-// import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
-// Vue.use(ElementUI);
-
-import '@/assets/styles/tailwind/index.css'
 
 new Vue({
   router,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="container">
-    <div class="flex flex-row w-10 h-10">
-      <div class="basis-1/4">01</div>
-      <div class="basis-1/4">02</div>
-      <div class="basis-1/2">03</div>
+  <div class="bg-gray-100">
+  <div class="flex justify-center items-center h-screen">
+    <div class="bg-white p-8 rounded shadow-md">
+      <h2 class="text-2xl font-bold mb-4">Login</h2>
+      <form>
+        <div class="mb-4">
+          <label for="email" class="block text-gray-700 font-bold mb-2">Tenant:</label>
+          <input type="email" id="email" name="email" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500" required>
+        </div>
+        <div class="mb-4">
+          <label for="email" class="block text-gray-700 font-bold mb-2">Account:</label>
+          <input type="email" id="email" name="email" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500" required>
+        </div>
+        <div class="mb-4">
+          <label for="password" class="block text-gray-700 font-bold mb-2">Password:</label>
+          <input type="password" id="password" name="password" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500" required>
+        </div>
+        <button type="submit" class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login</button>
+      </form>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -17,6 +32,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
