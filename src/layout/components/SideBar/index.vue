5d8778c2a9caa9e1a2860721cde6c54e5f369511
unknown <ramastarzhu01@gmail.com>
2023-12-23 21:51:09 +0800

    bug: 出现未知报错

--- src/layout/components/SideBar/index.vue
+++ src/layout/components/SideBar/index.vue
@@ -1,58 +1,101 @@
 <template>
-  <el-menu
-    class="no-scrollbar overflow-y-scroll bg-gray-600"
-    @open="handleOpen"
-    @close="handleClose"
+  <div
+    :class="{ 'has-logo': showLogo }"
+    :style="{
+      backgroundColor:
+        settings.sideTheme === 'theme-dark'
+          ? variables.menuBackground
+          : variables.menuLightBackground,
+    }"
   >
-    <el-menu-item>
-      <i class="el-icon-location float-left mt-19"></i>系统首页</el-menu-item
-    >
-    <el-submenu
-      class="hover:bg-gray-900"
-      v-for="i in menu"
-      :key="i.path"
-      :index="i.path"
-    >
-      <template slot="title">
-        <i class="el-icon-location float-left mt-19"></i>
-        <span class="text-white text-left">{{ i.meta.title }}</span>
-      </template>
-      <el-menu-item
-        class="bg-gray-600 text-white hover:bg-gray-900"
-        v-for="j in i.children.filter((i) => !i.hidden)"
-        :key="j.path"
-        :index="j.path"
-        >{{ j.meta.title }}</el-menu-item
+    <logo v-if="showLogo" :collapse="isCollapse" />
+    <div v-if="isStore" class="store-info">
+      <span class="name">{{ isStore }}</span>
+    </div>
+    <el-scrollbar :class="settings.sideTheme" wrap-class="scrollbar-wrapper">
+      <el-menu
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="
+          settings.sideTheme === 'theme-dark'
+            ? variables.menuBackground
+            : variables.menuLightBackground
+        "
+        :text-color="
+          settings.sideTheme === 'theme-dark'
+            ? variables.menuColor
+            : variables.menuLightColor
+        "
+        :unique-opened="true"
+        :active-text-color="settings.theme"
+        :collapse-transition="false"
+        mode="vertical"
       >
-    </el-submenu>
-  </el-menu>
+        <sidebar-item
+          v-for="(route, index) in sidebarRouters"
+          :key="route.path + index"
+          :item="route"
+          :base-path="route.path"
+        />
+      </el-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script>
-import { getRouters } from '@/api/menu'
+import { mapGetters, mapState } from "vuex";
+import Logo from "./Logo";
+import SidebarItem from "./SidebarItem";
+import variables from "@/assets/styles/variables.scss";
+
 export default {
-  data() {
-    return {
-      menu: []
-    }
-  },
-  created() {
-    this.getMenu()
-  },
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath)
+  components: { SidebarItem, Logo },
+  computed: {
+    ...mapState(["settings"]),
+    ...mapGetters(["sidebarRouters", "sidebar"]),
+    activeMenu() {
+      const route = this.$route;
+      const { meta, path } = route;
+      // if set path, the sidebar will highlight the path you set
+      if (meta.activeMenu) {
+        return meta.activeMenu;
+      }
+      return path;
     },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath)
+    showLogo() {
+      return this.$store.state.settings.sidebarLogo;
     },
-    getMenu() {
-      getRouters().then(({ data }) => {
-        if (data) {
-          this.menu = data.filter((item) => !item.hidden)
-        }
-      })
-    }
+    variables() {
+      return variables;
+    },
+    isCollapse() {
+      return !this.sidebar.opened;
+    },
+    isStore() {
+      if (this.$store.getters.storeName && this.$store.getters.storeId) {
+        return this.$store.getters.storeName;
+      }
+      return "";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.store-info {
+  height: 38px;
+  text-align: center;
+  padding-top: 10px;
+  background: #00acac;
+  overflow: hidden;
+  border-radius: 5px;
+  margin: 0px 6px 10px 6px;
+  .name {
+    font-weight: bold;
+    font-size: 13px;
+    color: #ffffff;
+    text-align: center;
+    overflow: hidden;
   }
 }
-</script>
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,27 @@
+'use strict'
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+
 const postcssConfig = require('./postcss.config.js')
 module.exports = {
   // vue项目中关闭eslint
   lintOnSave: false,
   publicPath: './',
+  configureWebpack: {
+    name: 'fuint会员营销管理系统',
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      },
+      fallback: {
+        "path": require.resolve("path-browserify")
+      }
+    },
+  },
   chainWebpack: config => {
     config.module
       .rule('css')
